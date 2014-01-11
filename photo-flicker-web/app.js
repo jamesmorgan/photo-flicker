c8856d5d5a3d77b4ad059c1456bedb8df23d51c7
James Morgan <james.morgan.e@googlemail.com>
2014-01-11 14:37:39 +0000

    Updated app.js

--- photo-flicker-web/app.js
+++ photo-flicker-web/app.js
@@ -145,7 +145,7 @@ app.use(errorHandler);
 /**
  * Start Server
  */
-
-http.createServer(app).listen(app.get('port'), function () {
-  console.log('Express server listening on port ' + app.get('port'));
-});
+http.createServer(app)
+    .listen(app.get('port'), function () {
+        console.log('Express server listening on port ' + app.get('port'));
+    });
