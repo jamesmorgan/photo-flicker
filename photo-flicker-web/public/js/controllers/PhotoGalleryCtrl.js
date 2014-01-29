6ae98db81528fa87f431e54d8ff009a30878a0c1
James Morgan <james.morgan.e@googlemail.com>
2014-01-29 15:44:50 +0000

    Removal of controller.js

--- photo-flicker-web/public/js/controllers/PhotoGalleryCtrl.js
+++ photo-flicker-web/public/js/controllers/PhotoGalleryCtrl.js
@@ -1,46 +1,6 @@
-'use strict';
 
-/* Controllers */
-
-angular.module('myApp.controllers', [])
-  .controller('NavMenuCtrl', function($scope, $log, GalleryService, GalleryModel, ScreenfullService) {
-
-        $scope.data = GalleryModel;
-
-        $scope.screenService = ScreenfullService;
-
-        $scope.$watch('data.selectedCategory', function(newVal, oldVal){
-            // Reset in preparation for the sub category selection
-            GalleryModel.selectedPhotos = [];
-            GalleryModel.selectInitialSubCategory();
-        }, true);
-
-        $scope.$watch('data.selectedSubCategory', function(newVal, oldVal){
-            GalleryModel.updatedPhotoSelection();
-        }, true);
-
-        $scope.$watch('data.searchResults', function(newVal, oldVal){
-            GalleryModel.updateFromSearch();
-        }, true);
-
-        $scope.goFullScreen = function(){
-            // ScreenfullService.fullScreen($('#main-window-container')[0]);
-            ScreenfullService.toggle($('.carousel-container')[0]);
-        };
-
-        $scope.exitFullScreen = function(){
-            ScreenfullService.exit();
-        };
-
-        $scope.lookupPhotos = function(val) {
-            return GalleryModel.queryPhotos(val);
-        };
-
-        $scope.updateFromSearch = function(){
-            GalleryModel.updateFromSearch();
-        };
-  })
-  .controller('PhotoGalleryCtrl', function($scope, $document, $log, GalleryService, ScreenfullService, GalleryModel, appConfig) {
+appControllers.controller('PhotoGalleryCtrl', 
+    function PhotoGalleryCtrl($scope, $document, $log, GalleryService, ScreenfullService, GalleryModel, appConfig) {
 
         $scope.debugMode = appConfig.debug;
         $scope.currentIndex = -1;
